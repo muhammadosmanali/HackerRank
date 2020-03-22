340a52ab7b049d3630d33bc7b8c40fb88318f768
Usman <muausman111@gmail.com>
2020-03-22 10:09:22 +0500

    loops

---
+++ 30 Days of Code/Day 5( Loops ).py
@@ -0,0 +1,15 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    n = int(input())
+
+    for x in range(1, 11):
+        print("{} x {} = {}".format(n, x, n*x))
