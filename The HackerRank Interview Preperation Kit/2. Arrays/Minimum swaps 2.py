d5a0b76fad8c779c8e1233f6328d69ca59874139
Usman <muausman111@gmail.com>
2020-03-29 19:41:11 +0500

    minimum swaps 2

---
+++ The HackerRank Interview Preperation Kit/2. Arrays/Minimum swaps 2.py
@@ -0,0 +1,33 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the minimumSwaps function below.
+def minimumSwaps(arr):
+    n = len(arr)
+    count = 0
+    for x in range(n):
+        while arr[x] != x+1:
+            temp = arr[x]
+            arr[x] = arr[temp-1]
+            arr[temp-1] = temp
+            count += 1
+    return count
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    res = minimumSwaps(arr)
+
+    fptr.write(str(res) + '\n')
+
+    fptr.close()
